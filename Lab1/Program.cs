85d0881f3f1de1ad821b39b415f36ba116954654
Jordan Massey <jmmhqw@mst.edu>
2024-01-31 09:41:19 -0600

    Finished lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+
+String name = Console.ReadLine() ?? string.Empty;
+
+while(name.Equals(string.Empty))
+{
+    Console.WriteLine("Please input a name");
+    name = Console.ReadLine() ?? string.Empty;
+}
+
+Console.WriteLine($"Noted: {name} got bread.");
