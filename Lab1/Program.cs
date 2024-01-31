6c455111a0f267dcde27314c852e27fe7f0b5085
Jordan Massey <jmmhqw@mst.edu>
2024-01-31 09:42:47 -0600

    Forgot to add comment

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Bread is ready.");
+﻿// IST 1551 Lab 1, Jordan Massey, 1/31/2024
+
+Console.WriteLine("Bread is ready.");
 Console.WriteLine("Who is the bread for?");
 
 String name = Console.ReadLine() ?? string.Empty;
