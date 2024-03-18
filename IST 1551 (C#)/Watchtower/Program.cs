6fdeaae311179bc72b606792fcb1c5f93e093bb8
Jordan Massey <jmmhqw@mst.edu>
2024-03-18 12:36:09 -0500

    Finished lab 6 (C#)

---
+++ IST 1551 (C#)/Watchtower/Program.cs
@@ -0,0 +1,17 @@
+﻿//IST 1551, Lab 6, Jordan Massey, March 18
+
+Console.Write("x: ");
+int x = int.Parse(Console.ReadLine() ?? "");
+Console.Write("y: ");
+int y = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine();
+
+if (x < 0  && y > 0)  Console.WriteLine("The enemy is to the north west!");
+if (x == 0 && y > 0)  Console.WriteLine("The enemy is to the north!");
+if (x > 0  && y > 0)  Console.WriteLine("The enemy is to the north east!");
+if (x < 0  && y == 0) Console.WriteLine("The enemy is to the west!");
+if (x == 0 && y == 0) Console.WriteLine("The enemy is here!");
+if (x > 0  && y == 0) Console.WriteLine("The enemy is to the east!");
+if (x < 0  && y < 0)  Console.WriteLine("The enemy is to the south west!");
+if (x == 0 && y < 0)  Console.WriteLine("The enemy is to the south!");
+if (x > 0  && y < 0)  Console.WriteLine("The enemy is to the south east!");
